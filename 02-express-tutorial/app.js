92626fa5e23e5a3d09936885d420a87efc6c527b
Sabare1 <sabaregirivasan@gmail.com>
2025-09-02 14:02:56 +0530

    started learning the basics express and it's methods

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,16 @@
-console.log('Express Tutorial')
+const express = require('express');
+const path = require('path')
+const server = express()
+server.use(express.static("./public"))
+
+server.get('/', (req, res)=>{
+    res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
+})
+
+server.all('*', (req, res)=>{
+    res.send(`<h1>Page not found</h1><a href='/'>home page</a>`)
+})
+
+server.listen(5000, ()=>{
+    console.log("server is listening on the port 5000...")
+})
